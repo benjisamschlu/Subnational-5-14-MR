ec47da7e3ab1f30456f8ad5c4e94174908acc3bc
benjisamschlu <benjamin-samuel.schluter@uclouvain.be>
2020-03-06 16:13:19 +0100

    selection of periods specific to each survey is done earlier in the data analysis process

--- functions/periodbreaks.R
+++ functions/periodbreaks.R
@@ -4,20 +4,36 @@
 
 # Code by: Benjamin-Samuel Schlüter
 # Date: 4/3/2020
+
+# Notes:
+# 1) set common period breaks but only keep three periods for each DHS
+# 2) amount.info allows to define if 1 year of data is sufficient to consider a period
+
 #-------------------------------------------------------------
 
-periodbreaks <- function(data, nber.p = 3, length.p = 4) {
+periodbreaks <- function(data, nber.p = 10, length.p = 4, amount.info = 1) {
         
-        enddate_DHS <- 1900 + max(data[[last_DHS]][,"v008"])/12
+        enddate_DHS <- 1900 + max(data[[last_DHS]][,"v008"])/12 # last interview date
         upper <- ifelse(abs(enddate_DHS - floor(enddate_DHS)) == 0, enddate_DHS, # if enddate_DHS after September (3/4 of a year) use it as highest year
                         ifelse(abs(enddate_DHS - floor(enddate_DHS)) >= 0.75, floor(enddate_DHS) + 1,
                                floor(enddate_DHS)))
-        
-        startdate_DHS <- 1900 + max(data[[head(filename1, 1)]]$v008)/12
-        lower <- ifelse(abs(startdate_DHS - floor(startdate_DHS)) == 0, startdate_DHS, 
-                        ifelse(abs(startdate_DHS - floor(startdate_DHS)) >= 0.75, floor(startdate_DHS) + 1,
-                               floor(startdate_DHS)))
-        lower <- lower - (nber.p*length.p) # defines lowest year for year.cut
+        lower <- upper - (nber.p*length.p) # defines lowest year for year.cut
         breaks <- sort( seq(upper, lower, by = -length.p) ) # start from latest year and go back in time (reason to have sort())
         breaks
+        
+        upper_date <- lapply(data, function(x) {1900 + range(x$v008)[2]/12}) # upper date DHS surveyS
+        y_cut <- list() # container for periods of each DHS
+        for (j in seq_along(filename1)) {
+                
+                time_lapse <- upper_date[[ filename1[j] ]] - head(breaks, -1) # time between lower bound period and upper date DHS
+                if (any(time_lapse >= 0 & time_lapse <= amount.info)) { # if upper date to close to lower bound of period, start periods one period below
+                        k <- which(time_lapse >= 0 & time_lapse <= amount.info)
+                        y_cut[[ filename1[j] ]] <- tail(breaks[1:k], 4)
+                } else {                                                # start periods in the period within which the upper date is
+                        k <- which.min(time_lapse[time_lapse>0])
+                        y_cut[[ filename1[j] ]] <- tail(breaks[1:k+1], 4)
+                }
+        }
+        y_cut
+        
 }
