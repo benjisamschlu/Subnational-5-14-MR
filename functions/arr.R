2f0a91b3548a812819185af495c99f7622246c00
Benjamin-Samuel Schlüter <benjamin-samuel.schluter@uclouvain.be>
2020-03-13 10:54:24 +0100

    improve arr() such that the output contains the period

--- functions/arr.R
+++ functions/arr.R
@@ -6,13 +6,15 @@
 # Date: 11/3/2020
 
 # Notes:
-# 1) 
+# 1) # This function will be used within mapply to loop on regions, 
+#      and whitin the function, it loops on the periods through 
+#      p.id
 
 #-------------------------------------------------------------
 
-arr <- function(data, region, age) {
+arr <- function(data, region, age) { 
         
-        df <- data[data$Type == "Space-Time Smoothing"& data$region == region, ]
+        df <- data[data$Type == "Space-Time Smoothing"& data$region == region, ] 
         df$p.id <- match(df$years, levels(df$years)) # get the periods order
         df <- midperiod(df) # own defined function to get mid period
         df$p.lab <- ifelse(df$p.mid <=2000, "< 2000", # defines the label to associate with arr
@@ -21,6 +23,7 @@ arr <- function(data, region, age) {
         
         iteration <- head(as.list(df$p.id), -1) # set the number of iteration -1 (ratio of f(t+1)/f(t))
         ARR <- unlist( lapply(iteration, function(x) { log(df$mean[df$p.id == x+1]/df$mean[df$p.id == x]) /  (df$p.mid[df$p.id == x] - df$p.mid[df$p.id == x+1])} ) ) # compute ARR looping on periods
+        period <- unlist (lapply(iteration, function(x) df$years[df$p.id == x+1]) )  # get period associated to ARR (t+1)
         label <- unlist (lapply(iteration, function(x) df$p.lab[df$p.id == x+1]) )  # get label associated to ARR (t+1)
-        return(data.frame(arr = ARR, period = label, region = region, age = age)) 
+        return(data.frame(arr = ARR, period = period, p.lab = label, region = region, age = age)) 
 }
